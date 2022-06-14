e0b5dc2dce3f678360bfad0b507b7562d5bfa6a0
ciferlu1024 <ciferlu1024@gmail.com>
2022-06-14 17:07:36 +0800

    change

--- redfish/chassis.go
+++ redfish/chassis.go
@@ -343,8 +343,21 @@ func GetChassis(c common.Client, uri string) (*Chassis, error) {
 
 	// 读取json文件获取json数据
 	jsonFile, err := os.Open("tmp/chassisjson.txt")
+	if err != nil {
+		fmt.Println("error opening json file")
+		return
+	}else{
+		fmt.Println("已打开json文件")
+	}
+
 	defer jsonFile.Close()
 	jsonData, err := ioutil.ReadAll(jsonFile)
+	if err!= nil {
+		fmt.Println("error reading json file")
+		return
+	}else{
+		fmt.Println("已读取json数据")
+	}
 
 	// 重新解析json数据
 
@@ -355,23 +368,24 @@ func GetChassis(c common.Client, uri string) (*Chassis, error) {
 	gobook, ok := r.(map[string]interface{})
 
 	if ok {
-	    for k, v := range gobook {
-	        switch v2 := v.(type) {
-	            case string:
-	                fmt.Println(k, "is string", v2)
-	            case int:
-	                fmt.Println(k, "is int", v2)
-	            case bool:
-	                fmt.Println(k, "is bool", v2)
-	            case []interface{}:
-	                fmt.Println(k, "is an array:")
-	                for i, iv := range v2 {
-	                    fmt.Println(i, iv)
-	                }
-	            default:
-	                fmt.Println(k, "is another type not handle yet")
-	        }
-	   }
+		fmt.Println("已解析json文件")
+		for k, v := range gobook {
+			switch v2 := v.(type) {
+				case string:
+					fmt.Println(k, "is string", v2)
+				case int:
+					fmt.Println(k, "is int", v2)
+				case bool:
+					fmt.Println(k, "is bool", v2)
+				case []interface{}:
+					fmt.Println(k, "is an array:")
+					for i, iv := range v2 {
+						fmt.Println(i, iv)
+					}
+				default:
+					fmt.Println(k, "is another type not handle yet")
+			}
+		}
 	}
 
 
