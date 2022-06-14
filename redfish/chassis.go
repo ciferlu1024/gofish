9ee15ebaa52447aca4b30334b0ebcc5ea85d5284
ciferlu1024 <ciferlu1024@gmail.com>
2022-06-14 16:22:55 +0800

    change

--- redfish/chassis.go
+++ redfish/chassis.go
@@ -338,7 +338,7 @@ func GetChassis(c common.Client, uri string) (*Chassis, error) {
 	//out.WriteTo(os.Stdout)
 
 	var r interface{}
-	err := json.Unmarshal(out.WriteTo(os.Stdout), &r)
+	_, err := json.Unmarshal(out.WriteTo(os.Stdout), &r)
 	fmt.Println("r", r)
 
 
